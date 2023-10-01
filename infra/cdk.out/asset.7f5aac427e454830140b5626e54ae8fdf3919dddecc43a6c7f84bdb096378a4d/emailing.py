8db49afc58b03ceb5ee3269e9741bb664402b259
Jackson Chen <imjacksonchen@gmail.com>
2023-10-01 00:54:01 -0700

    first commit

---
+++ infra/cdk.out/asset.7f5aac427e454830140b5626e54ae8fdf3919dddecc43a6c7f84bdb096378a4d/emailing.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+import os
+
+import openai
+
+def generate_email(prompt):
+    load_dotenv("keys.env")
+    openai.api_key = os.getenv("OPEN_AI_API_KEY")
+    response = openai.Completion.create(
+        engine = "davinci-instruct-beta-v3",
+        prompt = prompt,
+        temperature = 1,
+        max_tokens = 300 
+    )
+
+    generated_email: str = response["choices"][0]["text"]
+    generated_email = generated_email.strip()
+
+    return generated_email
+
+### Prompts ###
+
+def reach_out_email(recipient_name, recipient_title, recipient_company, sender_name, sender_company):
+    reach_out_prompt = f"Generate a personalized message for business outreach for {recipient_name}, {recipient_title}, {recipient_company}. My name is {sender_name}, {sender_company}. With no fill ins and under 300 words."
+    return generate_email(reach_out_prompt)
+    
+def reengagement_email(recipient_name, recipient_title, recipient_company, sender_name, sender_company):
+    reengagement_prompt = f"Generate a personalized message for re-engaging {recipient_name}, {recipient_title}, {recipient_company} about a business outreach. My name is {sender_name}, {sender_company}. With no fill ins and under 300 words."
+    return generate_email(reengagement_prompt)
+
+def feedback_email(recipient_name, recipient_title, recipient_company, sender_name, sender_company):
+    feedback_prompt = f"Generate a personalized message for {recipient_name}, {recipient_title}, {recipient_company} asking for feedback on our customer service. My name is {sender_name}, {sender_company}. With no fill ins and under 300 words."
+    return generate_email(feedback_prompt)
+
+
+# recipient_name = input("What is the recipient's name")
+# recipient_title = input("What is the recipient's title")
+# recipient_company = input("What is the recipient's company")
+
+# sender_name = input("What is the sender's name")
+# sender_company = input("What is the sender's company")
---
+++ infra/infra/infra_stack.py
@@ -0,0 +1,49 @@
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as api_gateway,
+)
+from constructs import Construct
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class InfraStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        layer = _lambda.LayerVersion(
+            self,
+            "BaseLayer",
+            code = _lambda.Code.from_asset("lambda_base_layer/layer.zip"),
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_11],
+            )
+
+        api_lambda = _lambda.Function(
+            self,
+            'ApiFunction',
+            runtime = _lambda.Runtime.PYTHON_3_11,
+            code = _lambda.Code.from_asset('../python-backend/'),
+            handler = "business_bot_api.handler",
+            layers = [layer],
+            environment = {
+                "OPENAI_API_KEY" : os.getenv("OPENAI_API_KEY") or "",
+                "SERPAPI_API_KEY" : os.getenv("SERPAPI_API_KEY") or "",
+                },
+        )
+
+        business_bot_api = api_gateway.RestApi(
+            self,
+            "RestApi",
+            rest_api_name = "Business bot API"
+        )
+
+        lambda_api_integration = api_gateway.LambdaIntegration(
+            api_lambda
+        )
+
+        business_bot_api.root.add_proxy(
+            default_integration = api_gateway.LambdaIntegration(api_lambda)
+        )
