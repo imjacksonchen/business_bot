c14556e5c06e781e3c05b3eee9e33490e18cc29d
Jackson Chen <imjacksonchen@gmail.com>
2023-10-03 16:10:53 -0700

    fix dependicies

--- r2d2_prototype/marketResearch.py
+++ r2d2_prototype/marketResearch.py
@@ -1,7 +1,8 @@
 import os
 
 from langchain.llms import OpenAI
-from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.agents import AgentType, initialize_agent, load_tools, Tool
+from langchain.utilities import SerpAPIWrapper
 
 # Use LangChain to get latest competitor information/research
 
@@ -9,8 +10,15 @@ def gather_intelligence(company):
     os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
     os.environ["SERPAPI_API_KEY"] = os.getenv("SERPAPI_API_KEY")
 
+    search = SerpAPIWrapper()
+
     llm = OpenAI(temperature=0)
-    tools = load_tools(["serpapi", "wikipedia"], llm=llm)
+    tools = load_tools([
+        Tool(
+        name="Search",
+        func=search.run,
+        description="useful for when you need to answer questions about current events",
+    )], llm=llm)
     agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = False)
 
     prompt = "What is the latest product from {}? What is the current sentiment of it? What is the latest news on them?".format(company)
