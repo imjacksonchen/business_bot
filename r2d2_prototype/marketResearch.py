2b7c95577ca775b7055ad6076e550fa92f20485b
Jackson Chen <imjacksonchen@gmail.com>
2023-10-03 16:24:26 -0700

    fix langchain

--- r2d2_prototype/marketResearch.py
+++ r2d2_prototype/marketResearch.py
@@ -11,10 +11,10 @@ def gather_intelligence(company):
     os.environ["SERPAPI_API_KEY"] = os.getenv("SERPAPI_API_KEY")
 
     llm = OpenAI(temperature=0)
-    tools = load_tools(["serpapi", "wikipedia"], llm=llm)
+    tools = load_tools(["serpapi"], llm=llm)
     agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = False)
 
-    prompt = "What is the latest product from {} and what is the news on it?".format(company)
+    prompt = "What is the latest news from {} ?".format(company)
 
     return agent.run(prompt)
 
